6e4750fa3f5b03927724fa1efaffc10eab0f8760
HamzaMiraz <amirhamza11170@gmil.com>
2025-08-21 00:38:10 +0600

    first

---
+++ TodoApp/WebApi/Controllers/EventsController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public EventsController(ApplicationDbContext context)
+        {
+            this.context=context;
+        }
+        [HttpGet]
+        public IActionResult GetEvents()
+        {
+            var events=context.Events.OrderByDescending(x => x.Id).ToList();
+            return Ok(events);
+
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetEvents(int id)
+        {
+            var evt = context.Events.Find(id);
+            if(evt == null)
+            {
+                return NotFound();
+            }
+            return Ok(evt);
+        }
+        [HttpGet]
+        public IActionResult CreateEvent(EventDto eventDto)
+        {
+            var evt = new Event
+            {
+                Title = eventDto.Title,
+                Description = eventDto.Description,
+                Start = eventDto.Start,
+                End = eventDto.End,
+                Allday = eventDto.Allday,
+                CreatedAt = DateTime.Now,
+            };
+            context.Events.Add(evt);
+            context.SaveChanges();
+            return Ok(evt);
+        }
+        [HttpPut("{id}")]
+        public IActionResult EditEvent(int id, EventDto eventDto)
+        {
+            var evt = context.Events.Find(id);
+            if(evt == null)
+            {
+                return NotFound();
+            }
+            evt.Title = eventDto.Title;
+            evt.Description = eventDto.Description;
+            evt.Start = eventDto.Start;
+            evt.End = eventDto.End;
+            evt.Allday = eventDto.Allday;
+            context.SaveChanges();
+            return Ok(evt);
+
+        }
+        [HttpDelete]
+        public IActionResult DeleteEvent(int id)
+        {
+            var evt = context.Events.Find(id);
+            if( evt == null)
+            {
+                return NotFound();
+            }
+            context.Events.Remove(evt);
+            context.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ TodoApp/WebApi/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Start {  get; set; }
+        public DateTime? End { get; set; }
+        public bool Allday { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
